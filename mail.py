3d9b45beed4bf01fd1ddbccc49544f2869208844
Adarshkumar14 <56665829+Adarshkumar14@users.noreply.github.com>
2021-04-18 17:32:44 +0530

    c2

--- mail.py
+++ mail.py
@@ -5,6 +5,6 @@ message = "Testing is Successfull!!,app is going to prod world"
 rec_email = "adarsh.adarshsingh12@gmail.com"
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login("doniksingh@gmail.com",""PaSS@1234)
+server.login("doniksingh@gmail.com","PaSS@1234")
 server.sendmail("Adarsh singh","adarsh.adarshsingh12@gmail.com",message)
 
